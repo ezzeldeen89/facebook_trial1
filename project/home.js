5900cf90ce4dd26bc20e16277c317b1bbeaf8583
ezzeldeen89 <69237171+ezzeldeen89@users.noreply.github.com>
2022-12-16 22:33:21 +0200

    Add files via upload

---
+++ project/home.js
@@ -0,0 +1,25 @@
+// var inptext;
+// function mypost(e,my_div) {
+//     inptext = document.getElementsByTagName("input")[1];
+//     inptext.addEventListener("focus",function () {
+//         if (inptext.value !== "") {
+
+//             var btn = document.createElement("button");
+//             // ele.textContent = "Hello"
+//             btn.textContent="post";
+//             document.body.appendChild(btn) //append last 
+//             // document.body.insertBefore(btn, my_div)
+//         }
+//     })
+// }
+
+
+
+
+var lik;
+var lik_col;
+function like(e){
+    lik=document.getElementById("div_like");
+    lik_col=lik.style.color;
+    
+}
